47254a81505f14ebd9d19365844cf2549b196099
daavsnts <daavsantosl@gmail.com>
2023-04-28 04:09:17 -0300

    feat: disable dynamic colors temporarily

--- app/src/main/java/com/daavsnts/mymovies/ui/theme/Theme.kt
+++ app/src/main/java/com/daavsnts/mymovies/ui/theme/Theme.kt
@@ -41,10 +41,10 @@ fun MyMoviesTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
