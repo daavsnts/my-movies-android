14c385a6654ce584dcf596df75a1f71fb9313038
daavsnts <daavsantosl@gmail.com>
2023-04-28 03:22:03 -0300

    feat: add Material ColorScheme

--- app/src/main/java/com/daavsnts/mymovies/ui/theme/Color.kt
+++ app/src/main/java/com/daavsnts/mymovies/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.daavsnts.mymovies.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val EerieBlack = Color(0xFF1E1B18)
+val GhostWhite = Color(0xFFFFFAFF)
+val Emerald = Color(0xFF23CE6B)
+val PersianRed = Color(0xFFCC2936)
+val Maize = Color(0xFFFFED66)
+val Night = Color(0xFF161412)
+val Jet = Color(0xFF2D2925)
