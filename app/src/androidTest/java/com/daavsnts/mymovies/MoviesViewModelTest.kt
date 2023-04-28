7d6d162e42b56b19995c09099b49a1162ad3aa58
daavsnts <daavsantosl@gmail.com>
2023-04-28 06:34:36 -0300

    test: fix MoviesViewModel tests

--- app/src/androidTest/java/com/daavsnts/mymovies/MoviesViewModelTest.kt
+++ app/src/androidTest/java/com/daavsnts/mymovies/MoviesViewModelTest.kt
@@ -6,6 +6,7 @@ import com.daavsnts.mymovies.model.Movie
 import com.daavsnts.mymovies.repository.NetworkMoviesRepository
 import com.daavsnts.mymovies.ui.screens.ScreenUiState
 import com.daavsnts.mymovies.ui.screens.movies.MoviesViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
@@ -17,36 +18,31 @@ class MoviesViewModelTest {
 
     @Test
     fun testMoviesUiStateList() = runBlocking {
+        delay(1000)
         val moviesUiStateList =
-            moviesViewModel.moviesUiStatesList.map {
-                Pair(
-                    it.title,
-                    it.list.first()
-                )
-            }
-        moviesUiStateList.forEach {
-            val movieUiStateList = it.second
-            if (movieUiStateList is ScreenUiState.Success)
-                assert(movieUiStateList.data.isNotEmpty())
+            moviesViewModel.moviesUiStatesList.map { it.list.first() }
+        moviesUiStateList.forEach { movieUiStateList ->
+            assert(movieUiStateList is ScreenUiState.Success)
         }
-        assert(true)
     }
 
     @Test
     fun testSearchedMoviesUiState() = runBlocking {
-        val searchedMoviesUiStateList = moviesViewModel.searchedMoviesUiState.first()
         moviesViewModel.setSearchedMoviesList("Fight Club")
-        val fightClubMovie = Movie(550,
+        delay(1000)
+
+        val searchedMoviesUiStateList = moviesViewModel.searchedMoviesUiState.first()
+        val fightClubMovie = Movie(
+            550,
             "Fight Club",
             "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
             releaseDate = "10/15/1999",
             posterPath = "https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
             voteAverage = 8.433f,
-            voteCount = 26280,
+            voteCount = 26288,
             genres = null
         )
         if (searchedMoviesUiStateList is ScreenUiState.Success)
             assert(searchedMoviesUiStateList.data.contains(fightClubMovie))
-        assert(true)
     }
 }
