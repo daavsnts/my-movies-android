1afba954695b51fb0edb4489f93e238b29b5d75f
daavsnts <daavsantosl@gmail.com>
2023-04-29 17:12:17 -0300

    refactor: remove some warnings

--- app/src/main/java/com/daavsnts/mymovies/ui/theme/Theme.kt
+++ app/src/main/java/com/daavsnts/mymovies/ui/theme/Theme.kt
@@ -1,22 +1,14 @@
 package com.daavsnts.mymovies.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     background = Jet,
