48ffbeed510c8049b7a3588afc89f58f51ed1632
daavsnts <daavsantosl@gmail.com>
2023-05-16 00:39:06 -0300

    feat: add MissingPoster Composable

---
+++ app/src/main/java/com/daavsnts/mymovies/ui/screens/composables/MissingPoster.kt
@@ -0,0 +1,23 @@
+package com.daavsnts.mymovies.ui.screens.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.daavsnts.mymovies.R
+
+@Composable
+fun MissingPoster(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = R.drawable.missing_poster),
+        contentDescription = stringResource(id = R.string.loading_error_content_description),
+        contentScale = ContentScale.Crop,
+        modifier = modifier.fillMaxSize().alpha(0.5f)
+    )
+}
