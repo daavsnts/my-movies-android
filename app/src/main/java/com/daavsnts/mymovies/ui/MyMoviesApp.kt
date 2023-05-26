1fb599495ec7f23b57a52a6f42fcace7e96ee1a5
daavsnts <daavsantosl@gmail.com>
2023-05-26 01:02:40 -0300

    refactor: remove some warnings from files

--- app/src/main/java/com/daavsnts/mymovies/ui/MyMoviesApp.kt
+++ app/src/main/java/com/daavsnts/mymovies/ui/MyMoviesApp.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import com.daavsnts.mymovies.ui.navigation.BottomNavBar
 import com.daavsnts.mymovies.ui.navigation.NavGraph
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
--- app/src/main/java/com/daavsnts/mymovies/ui/screens/movies/MoviesScreen.kt
+++ app/src/main/java/com/daavsnts/mymovies/ui/screens/movies/MoviesScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.daavsnts.mymovies.ui.screens.composables.MoviesGrid
 import com.daavsnts.mymovies.ui.screens.composables.SearchBar
 import com.daavsnts.mymovies.domain.model.Movie
